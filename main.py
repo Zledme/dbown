6f5e446408730b1961532dd1b72cc99257de8607
zled <zledme@tutanota.com>
2022-06-22 02:09:36 +0530

    imp

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import os
 import requests
 import argparse
+import hashlib, binascii
 
 BASE_URL = 'https://api.openweathermap.org/data/2.5/'
 KEY = '34f4a8299883147d27107370411a142c'
@@ -21,15 +22,34 @@ parser.add_argument('--getbycn',nargs=1,metavar='city',help='get the data throug
 args = parser.parse_args()
 
 
+#hashing password
+def hash_password(password):
+    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    password_hash = hashlib.pbkdf2_hmac('sha512',password.encode('utf-8'),
+    salt,100000)
+    password_hash = binascii.hexlify(password_hash)
+    return (salt+password_hash).decode('ascii')
+
+def check_password(stored_password, user_password):
+    salt = stored_password[:64]
+    stored_password = stored_password[64:]
+    password_hash = hashlib.pbkdf2_hmac('sha512',user_password.encode('utf-8'),
+    salt.encode('ascii'),
+    100000)
+    password_hash = binascii.hexlify(password_hash).decode('ascii')
+    return password_hash == stored_password
+
+
 
 #login
 def login():
     user = input("Name: ")
     passw = input("Password: ")
-    for line in open("user.txt","r").readlines():
+    for line in open("users.txt","r").readlines():
         usr = line.split()
         if usr[0] == user:
-            print('user found')    
+            return check_password(usr[1], passw)
+    return False
 
 #create new user
 def create():
@@ -37,20 +57,44 @@ def create():
     name = input('Name:')
     passw = input('Password:')
     with open('users.txt', 'w') as f:
-        f.write(name+' '+hashed_password+'\n')
+        f.write(name+' '+hash_password(passw)+'\n')
 
 #update the user
 def update():
     print('Update the password')
+    user = input('Enter the user:')
     passw1 = input('Enter New Password:')
-    passw2 = input('Re-enter New Password:')
-    with open('users.txt', 'w') as f:
-        f.write('Logged in')
+    checkexist = False
+    with open('users.txt', 'r') as f:
+        lines = f.readlines()
 
+        with open('users.txt','w') as fw:
+            for line in lines:
+                usr = line.split()
+                if usr[0] != user:
+                    fw.write(line)
+                if usr[0] == user:
+                    checkexist = True
+                    fw.write(user+' '+hash_password(passw1)+'\n')
+             
+    if not checkexist:
+        print("user doesn't exist")
 #delete a user
 def delete():
     name = input('Enter the User to delete:')
+    checkexist = False
+    with open('users.txt', 'r') as f:
+        lines = fr.readlines()
 
+        with open('months.txt','w') as fw:
+            for line in lines:
+                usr = line.split()
+                if usr[0] != user:
+                    f.write(line)
+                if usr[0] == user:
+                    checkexist = True
+    if not checkexist:
+        print("user doesn't exist")
 #getdata
 def getdata(query_params):
     response = requests.get(f'{BASE_URL}/weather',params=query_params)
@@ -67,7 +111,7 @@ def getdata(query_params):
 
 #call the api
 def show_all():
-    for line in open("user.txt","r").readlines():
+    for line in open("users.txt","r").readlines():
         usr = line.split()
         print(usr[0])
 
@@ -89,6 +133,7 @@ def main():
                     with open('login.txt', 'w') as f:
                         f.write('Logged in')
                     exit()
+                chances -=1
             print('Retry the command')
 
 
